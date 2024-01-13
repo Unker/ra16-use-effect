e8593846317bd3f526233a0755840d5b616d2780
unker <kfi.unknown@gmail.com>
2024-01-13 21:39:16 +0300

    added style btn

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -7,6 +7,7 @@ interface ListProps {
 
 const List: React.FC<ListProps> = ({ onSelectUser }) => {
   const [users, setUsers] = useState<User[]>([]);
+  const [selectedUserId, setSelectedUserId] = useState<number | null>(null);
 
   useEffect(() => {
     // Загрузка пользователей
@@ -15,12 +16,21 @@ const List: React.FC<ListProps> = ({ onSelectUser }) => {
       .then((data) => setUsers(data));
   }, []);
 
+  const handleSelectUser = (user: User) => {
+    onSelectUser(user);
+    setSelectedUserId(user.id);
+  };
+
   return (
     <div>
       <h2>User List</h2>
       <ul>
         {users.map((user) => (
-          <li key={user.id} onClick={() => onSelectUser(user)}>
+          <li 
+            key={user.id} 
+            onClick={() => handleSelectUser(user)}
+            className={selectedUserId === user.id ? 'selected-user' : ''}
+          >
             {user.name}
           </li>
         ))}
